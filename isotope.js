ec32bae72e8479baf1498a35dbedc5f2af67a28d
Kacie Guthrie <kacie@Kacies-MacBook-Pro.local>
2019-04-29 12:44:58 -0700

    desktop styles

--- isotope.js
+++ isotope.js
@@ -9,7 +9,7 @@ var qsRegex;
 
 
 // init Isotope
-var $grid = $('.thumbnails').isotope({
+var $grid = $('.grid').isotope({
   itemSelector: '.element-item',
   getSortData: {
     name: '.name',
@@ -79,18 +79,12 @@ function debounce( fn, threshold ) {
   };
 }
 
-
 // bind sort button click
 $('.sort-by-button-group').on( 'click', 'button', function() {
   var sortValue = $(this).attr('data-sort-value');
   $grid.isotope({ sortBy: sortValue });
 });
 
-// change is-checked class on buttons
-// $('.button-group').each( function( i, buttonGroup ) {
-//   var $buttonGroup = $( buttonGroup );
-//   $buttonGroup.on( 'click', 'button', function() {
-//     $buttonGroup.find('.is-checked').removeClass('is-checked');
-//     $( this ).addClass('is-checked');
-//   });
-// });
+
+
+//hide navbar
